bcae53cff3b72a82bd995cfcb9af86b33fecc5aa
SummerPaX <summerpax@outlook.at>
2022-04-28 04:25:37 +0200

    remove account route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/dashboard-view.vue";
 import Lists from "../views/lists-view.vue";
 import Recipes from "../views/recipes-view.vue";
-import Account from "../views/account-view.vue";
 import Settings from "../views/settings-view.vue";
 
 const routes = [
   { path: "/lists", name: "Lists", component: Lists },
   { path: "/recipes", name: "Recipes", component: Recipes },
-  { path: "/account", name: "Account", component: Account },
   { path: "/settings", name: "Settings", component: Settings },
+  { path: '/logout', redirect: '/' },
   { path: "/:pathMatch(.*)*", name: "Dashboard", component: Dashboard },
 ];
 
